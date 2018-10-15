1a9eda638a61d1b8685a70d9ae2f0ddada89df4c
fleurg <f.gawehns-bruning@nioo.knaw.nl>
2018-10-15 21:42:25 +0200

    make more general

--- demultiplex.sh
+++ demultiplex.sh
@@ -1,8 +1,8 @@
 python2 ../src/epiGBS/demultiplex.py \
---r1_in ../data/demult-files/"${barcode}"-1.fq.gz \
---r2_in ../data/demult-files/"${barcode}"-2.fq.gz \
+--r1_in ../data/file1-R1.fq.gz \
+--r2_in ../data/file1-R2.fq.gz \
 --barcodes ../data/barcodes.tsv \
 --output-dir ./demultiplex_test_data \
- --addRG -d --nomatch1 ./demultiplex_test_data/no-match."${barcode}"-1.fastq.gz \
- --nomatch2 ./demultiplex_test_data/no-match."${barcode}"-2.fastq.gz \
- --stat ./demultiplex_test_data/demultiplex."${barcode}"-stat.tsv
+ --addRG -d --nomatch1 ./demultiplex_test_data/no-match.file1-R1.fastq.gz \
+ --nomatch2 ./demultiplex_test_data/no-match.file1-R2.fastq.gz \
+ --stat ./demultiplex_test_data/demultiplex.file1-stat.tsv
